b7d13ebba7264f2db320eca475bdbcf8a9e31334
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2023-01-13 19:59:27 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
+const { createApp } = Vue
+      
+createApp({
+        data() {
+                return {
+                        newTask: '',
+                        characters: [
+
+                        ]
+                }
+        },
+        methods: {
+                add() {
+                        if (!this.newTask) return //ak sa na neyplní políčko s úlohou, metóda add končí
+
+                        this.characters.push(
+                                this.newTask //premennú newTask natlač do characters
+                        )
+
+                        this.newTask = '' //po pridaní novej úlohy zostane input prázdny
+
+                        this.$refs.new.focus() //po pridaní novej úlohy sa kurzor automaticky nastaví na input
+                },
+                remove(newTask) {
+                        this.characters = this.characters.filter(item => item !== newTask) //odfiltruj z characters všetky itemy ktoré nie sú newTask
+        },
+    }
+}).mount('#app')
+
+
+
+
+
+
+
+
+
+
+
 $('.deletedTasks').on('click', function() {
         $('.activeTasks').removeClass('active text-primary');
         $('.activeTasks').addClass('text-secondary');
