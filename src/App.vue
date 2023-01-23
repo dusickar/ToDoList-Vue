6e40a803064ffe47d33a06f2eef7159d973c6012
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2023-01-23 19:49:29 +0100

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
-  <NewTask>
-
-  </NewTask>
+  <div>
+    <ProjectName />
+    <NewTask />
+    <ActiveTask />
+    <DeletedTask />
+  </div>
 </template>
 
 
 <script>
+import ProjectName from "./components/ProjectName.vue"
 import NewTask from './components/NewTask.vue'
+import ActiveTask from './components/ActiveTask.vue'
+import DeletedTask from './components/DeletedTask.vue'
 
 export default {
   name: 'App',
   components: {
-    NewTask
+    ProjectName ,
+    NewTask ,
+    ActiveTask ,
+    DeletedTask
   },
 }
 </script>
 
 
 
-<style scoped>
+
+<style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,6 +37,16 @@ export default {
   color: #2c3e50;
 }
 
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
-
-
