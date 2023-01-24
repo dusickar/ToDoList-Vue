639424044b24ea4722d6deebf101d62afd3a42dc
Dušan Riečičiar <81569486+dusickar@users.noreply.github.com>
2023-01-24 19:05:47 +0100

    Add files via upload

---
+++ src/views/activepage.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="nav-item">
+      <a  class="nav-link" aria-current="true" href="#">Aktívne úlohy</a>
+  </li>
+</template>
+  
+  
+  <script>
+  // @ is an alias to /src
+  import ActiveTask from '../components/ActiveTask.vue'
+  export default {
+    name: 'App',
+    components: {
+      ActiveTask
+    },
+  }
+  </script>
---
+++ src/views/deletedpage.vue
@@ -0,0 +1,17 @@
+<template>
+    <li class="nav-item">
+        <a  class="nav-link" href="#">Zmazané úlohy</a>
+    </li>
+</template>
+  
+  
+  <script>
+  // @ is an alias to /src
+  import DeletedTask from '../components/DeletedTask.vue'
+  export default {
+    name: 'App',
+    components: {
+      DeletedTask
+    },
+  }
+  </script>
